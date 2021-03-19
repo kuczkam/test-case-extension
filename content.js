c1c1e3fe9d5f22bb7dde0192c17ea6dda7344057
kuczkam <42166328+kuczkam@users.noreply.github.com>
2021-03-19 11:52:15 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 function runIt() {
     const testCaseContainer = document.createElement('div');
     const ul = document.createElement('ul');
+    const h1 = document.createElement('h1');
 
     var cssId = 'tc-css';
     if (!document.getElementById(cssId))
@@ -10,12 +11,14 @@ function runIt() {
         link.id   = cssId;
         link.rel  = 'stylesheet';
         link.type = 'text/css';
-        link.href = 'https://gitcdn.link/repo/kuczkam/do/master/style.css';
+        link.href = 'https://gitcdn.link/repo/kuczkam/test-case-extension/main/popup.css';
         link.media = 'all';
         head.appendChild(link);
     }
 
     testCaseContainer.setAttribute('class', 'tc-container');
+    h1.innerText = "go to " + window.location.href;
+    testCaseContainer.appendChild(h1);
     ul.setAttribute('class', 'tc-list');
     testCaseContainer.appendChild(ul);
     document.body.appendChild(testCaseContainer);
@@ -23,13 +26,22 @@ function runIt() {
     const createElement = (e) => {  
         const li = document.createElement('li');
         const tcList = document.querySelector('.tc-list');
+        const btnPass = document.createElement('BUTTON');
+        const btnFail = document.createElement('BUTTON');
         const elem = e.target;
         
-        li.innerText = elem.outerHTML;
+        btnPass.setAttribute("id", "tc-pass");
+        btnFail.setAttribute("id", "tc-fail");
+        li.innerText = "- click on " + elem.outerHTML;
+        btnPass.innerText = "pass";
+        btnFail.innerText = "fail";
+        li.appendChild(btnPass);
+        li.appendChild(btnFail);
         tcList.appendChild(li);
     }
 
-    document.addEventListener('click', createElement)
+    document.addEventListener('click', createElement);
+
     
 }
 runIt();
