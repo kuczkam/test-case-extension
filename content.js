03bf7e616cb044a6001451cd9ff1f3581a2ebfb9
kuczkam <42166328+kuczkam@users.noreply.github.com>
2021-03-21 21:40:29 +0100

    Update content.js
    css, result and typing

--- content.js
+++ content.js
@@ -1,53 +1,71 @@
-function runIt() {
-    const testCaseContainer = document.createElement('div');
-    const ul = document.createElement('ul');
-    const h1 = document.createElement('h1');
+function testCaseRun() {
+    const testCaseContainer = document.createElement("div");
+    const ul = document.createElement("ul");
+    const h1 = document.createElement("h1");
+    const css = ".tc-container{position:fixed;left:-343px;top:10%;width:382px;min-height:300px;background:#d80348;z-index:100;transition:1s;}.tc-container:hover{left:0;transition:1s;}.tc-list > li{color:#fff;padding:5px;font-size:12px;}";
 
+    const body  = document.getElementsByTagName("body")[0];
+    const style  = document.createElement("style");
+    style.innerText = css;
+    body.appendChild(style);
 
-    var cssId = 'tc-css';
-    if (!document.getElementById(cssId))
-    {
-        var head  = document.getElementsByTagName('head')[0];
-        var link  = document.createElement('link');
-        link.id   = cssId;
-        link.rel  = 'stylesheet';
-        link.type = 'text/css';
-        link.href = 'https://gitcdn.link/repo/kuczkam/test-case-extension/main/popup.css';
-        link.media = 'all';
-        head.appendChild(link);
-    }
-
-    testCaseContainer.setAttribute('class', 'tc-container');
+    testCaseContainer.setAttribute("class", "tc-container");
     h1.innerText = "go to " + window.location.href;
     testCaseContainer.appendChild(h1);
-    ul.setAttribute('class', 'tc-list');
+    ul.setAttribute("class", "tc-list");
     testCaseContainer.appendChild(ul);
     document.body.appendChild(testCaseContainer);
 
-    const createElement = (e) => {  
+    const tcResult = (e, tcResult) => {
+        const item = e.target.closest("li");
+        const mapObj = {
+            click: `result(${tcResult}) visible element:`,
+            on: "",
+            pass: "",
+            fail: ""
+        };
+        const result = item.innerText.replace(/click|on|pass|fail/gi, (matched) => {
+            return mapObj[matched];
+        });
 
-        if (e.target.id != "tc-pass") {
-            const li = document.createElement('li');
-            const tcList = document.querySelector('.tc-list');
-            const pass = document.createElement('BUTTON');
-            const elem = e.target;
-            
-            pass.setAttribute("id", "tc-pass");
-            pass.setAttribute('style', "padding-left: 5px; cursor: pointer");
-            pass.innerText = "pass";
-            li.innerText = "- click on " + elem.outerHTML;
-            li.appendChild(pass);
-            tcList.appendChild(li);
-        }
-
-        if (e.target.id == "tc-pass") {
-            console.log('juhuuu');
-        }
-
+        item.innerText = result;
     }
 
-    document.addEventListener('click', createElement);
+    const createElement = (e, value) => {  
 
-    
+        if (e.target.id != "tc-pass" && e.target.id != "tc-fail") {
+            const li = document.createElement("li");
+            const tcList = document.querySelector(".tc-list");
+            const pass = document.createElement("BUTTON");
+            const fail = document.createElement("BUTTON");
+            
+            pass.setAttribute("id", "tc-pass");
+            fail.setAttribute("id", "tc-fail");
+            pass.setAttribute("style", "padding-left: 5px; cursor: pointer");
+            fail.setAttribute("style", "padding-left: 5px; cursor: pointer");
+            pass.innerText = "pass";
+            fail.innerText = "fail";
+            if (e.target.type == "text") {
+                setTimeout(() => {
+                    li.innerText = `- type (${e.target.value}) in the ` + value;
+                }, 3000)
+            } else {
+                li.innerText = "- click on " + value;
+            }
+            li.appendChild(pass);
+            li.appendChild(fail);
+            tcList.appendChild(li);
+        }
+    }
+
+    document.addEventListener("click", (e) => {
+        createElement(e, e.target.outerHTML);
+        if (e.target.id == "tc-pass") {
+            tcResult(e, "pass");
+        }
+        if (e.target.id == "tc-fail") {
+            tcResult(e, "fail");
+        }
+    });   
 }
-runIt();
+testCaseRun();
